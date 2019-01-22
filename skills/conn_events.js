34d85e9ebc9760ca774da02450b074e6967c65bc
Valentina Adami <valeadami@gmail.com>
2019-01-22 13:05:56 +0100

    le skills

---
+++ skills/conn_events.js
@@ -0,0 +1,30 @@
+module.exports = function(controller) {
+
+
+    // if (process.env.studio_token) {
+       controller.on('hello', function(bot, message) {
+             // a new session with an unknown user has begun welcome_user
+           //  controller.studio.run(bot, 'tutorial', message.user, message.channel, message);
+           bot.reply(message,'benvenuto, sono il bot dell\' università di Trieste');
+       });
+ 
+       controller.on('welcome_back', function(bot, message) {
+           // a known user has started a new, fresh session
+          /* controller.studio.run(bot, 'welcome_user', message.user, message.channel, message).then(function(convo) {
+               convo.gotoThread('welcome_back');
+           })*/
+           bot.reply(message,'benvenuto, sono il bot dell\' università di Trieste');
+       });
+ 
+      /* controller.studio.before('welcome_user', function(convo, next) {
+           convo.setVar('bot', controller.studio_identity);
+           next();
+       });*/
+ 
+       controller.on('reconnect', function(bot, message) {
+           // the connection between the client and server experienced a disconnect/reconnect
+            bot.reply(message, 'Some sub-space interference just caused our connection to be interrupted. But I am back now.');
+       });
+     }
+ 
+ //}
---
+++ skills/skill_libretto.js
@@ -0,0 +1,26 @@
+module.exports = function(controller) {
+
+    controller.hears('libretto','message_received', function(bot, message) {
+  
+      bot.reply(message, {
+          text: 'Sono nel libretto!',
+          quick_replies: [
+              {
+                  title: 'Elenco esami',
+                  payload: 'Elenco esami'
+              },
+              {
+                  title: 'Esami sostenuti anno scorso',
+                  payload: 'Esami sostenuti anno scorso'
+              },
+              {
+                title: 'Esami sostenuti questo anno',
+                payload: 'Esami sostenuti questo anno'
+            }
+          ]
+        },function() {});
+  
+  
+        });
+  
+  }
